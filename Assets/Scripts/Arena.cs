39ceb4699ad8e9833b53be4c3999481bb8c192d6
Tore Bergebakken <toberge@stud.ntnu.no>
2023-04-16 19:17:48 +0200

    Upscale arena again

--- Assets/Scripts/Arena.cs
+++ Assets/Scripts/Arena.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Arena
 {
-    public const float Width = 32;
+    public const float Width = 48;
     public const float EnemyHalfWidth = Width / 2 - 1;
     public const float EnemySpawnHalfWidth = Width / 2 + 1;
-    public const float Height = 16;
+    public const float Height = 27;
     public const float EnemyHalfHeight = Height / 2 - 1;
     public const float EnemySpawnHalfHeight = Height / 2 + 1;
 

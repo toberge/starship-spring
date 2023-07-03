875462e1c0e0b27a23d4d8b08f6c9a7680d4160d
Tore Bergebakken <toberge@stud.ntnu.no>
2023-07-03 15:49:43 +0200

    Hide powerup animation bug

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -31,8 +31,6 @@ public class Powerup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // TODO alternatively do it from component on ship
-        //      (overlapping times make this kinda thing necessary)
         if ((playerLayer.value & (1 << other.gameObject.layer)) == 0)
             return;
 
@@ -44,13 +42,18 @@ public class Powerup : MonoBehaviour
 
     private void Apply(Ship ship)
     {
+        GetComponent<Rigidbody2D>().simulated = false;
+        GetComponent<Collider2D>().enabled = false;
+
         audioSource?.Play();
-        shell.gameObject.LeanScale(3f * Vector3.one, 0.2f);
-        LeanTween.value(shell.gameObject, SetAlpha, 1, 0, 0.25f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => Destroy(shell.gameObject));
-        content.gameObject.LeanScale(Vector3.zero, 0.2f).setOnComplete(() => Destroy(content.gameObject));
+        var sequence = LeanTween.sequence();
+        sequence.append(LeanTween.scale(shell.gameObject, 3f * Vector3.one, 0.2f));
+        sequence.append(LeanTween.value(shell.gameObject, SetAlpha, 1, 0, 0.1f)
+            .setEase(LeanTweenType.easeOutQuad)
+            .setOnComplete(() => Destroy(shell.gameObject)));
+        LeanTween.scale(content.gameObject, Vector3.zero, 0.2f).setOnComplete(() => Destroy(content.gameObject));
 
         effect.Apply(ship);
-        GetComponent<Rigidbody2D>().simulated = false;
         StartCoroutine(WearOffEffect(ship));
     }
 
@@ -61,7 +64,11 @@ public class Powerup : MonoBehaviour
 
     private IEnumerator WearOffEffect(Ship ship)
     {
-        yield return new WaitForSecondsRealtime(duration);
+        // Mask that one weird bug where the shell or content lingers
+        yield return new WaitForSecondsRealtime(0.31f);
+        if (shell) Destroy(shell.gameObject);
+        if (content) Destroy(content.gameObject);
+        yield return new WaitForSecondsRealtime(duration - 0.31f);
         effect.Remove(ship);
         Destroy(gameObject);
     }
